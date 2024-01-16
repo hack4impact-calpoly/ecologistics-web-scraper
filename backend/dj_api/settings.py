b53e858d2760e13d6579c2a89aaac665bc9bb61f
oli-lane <markus.oliver.lane@gmail.com>
2024-01-15 20:00:15 -0800

    feat: mongo connection

--- backend/dj_api/settings.py
+++ backend/dj_api/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'dj_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'mongo_db',
+#     }
+# }
 
 
 # Password validation
