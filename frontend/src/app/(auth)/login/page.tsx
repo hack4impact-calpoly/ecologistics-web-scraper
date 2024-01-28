5135762e9743643057665f8bc91ed0e1b0e72e78
Hayley <haysho2260@gmail.com>
2024-01-28 10:33:18 -0800

    update login to not use bootstrap and use radcn

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import styles from "../auth.module.css";
-import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
-const LoginPage = () => {
-  const router = useRouter();
+export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordShown, setPasswordShown] = useState(false);
@@ -13,100 +20,62 @@ const LoginPage = () => {
   const togglePasswordShown = () => {
     setPasswordShown(!passwordShown);
   };
-  const navigateToSignUp = () => {
-    router.push("/signup"); // Replace "/signup" with the actual path of your signup page
-  };
 
   const handleLogin = () => {
     // CHANGE THIS TO NAVIGATE TO NEW PAGE
-    console.log("Email", email)
-  }
+    console.log("Email", email);
+  };
 
   return (
-    <section className="vh-100" style={{ backgroundColor: "#508bfc" }}>
-      <div className="container py-5 h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div
-              className="card shadow-2-strong"
-              style={{ borderRadius: "1rem" }}
-            >
-              <div className="card-body p-5 text-center">
-                <h3 className={`mb-5 ${styles.myHeading}`}>Log In</h3>
-
-                <div className="form-outline mb-4">
-                  <input
-                    type="email"
-                    id="typeEmailX-2"
-                    className="form-control"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <label className="form-label" htmlFor="typeEmailX-2">
-                    Email
-                  </label>
-                </div>
-
-                <div className="form-outline mb-4">
-                  <div>
-                    <input
-                      type={passwordShown ? "text" : "password"}
-                      id="typePasswordX-2"
-                      className="form-control"
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <label className="form-label" htmlFor="typePasswordX-2">
-                      Password
-                    </label>
-                  </div>
-                  <div>
-                    <a
-                      className={`${styles.forgotPassword}`}
-                      role="button"
-                      href="/your-destination-page"
-                    >
-                      Forgot Password?
-                    </a>
-                  </div>
-                </div>
-                <div className="form-check d-flex justify-content-start mb-4">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    checked={passwordShown}
-                    onChange={togglePasswordShown}
-                    id="form1Example3"
-                  />
-                  <label
-                    className={`form-check-label ${styles.formCheckLabel}`}
-                    htmlFor="form1Example3"
-                  >
-                    Show Password
-                  </label>
-                </div>
-
-                <button
-                  className={`${styles.signup}`}
-                  type="submit"
-                  onClick={handleLogin}
-                >
-                  Login
-                </button>
-                <div>
-                  <a
-                    className={`${styles.newAccount}`}
-                    role="button"
-                    onClick={navigateToSignUp}
-                  >
-                    Create New Account
-                  </a>
-                </div>
-              </div>
+    <Card className="w-[370px] p-6 mx-auto">
+      <CardHeader>
+        <CardTitle className="text-2xl text-gray-700 pb-1 text-center">
+          Log In
+        </CardTitle>
+        {/* <CardDescription>Deploy your new project in one-click.</CardDescription> */}
+      </CardHeader>
+      <CardContent className="pb-4">
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Email</Label>
+              <Input
+                id="name"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type={passwordShown ? "text" : "password"}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
           </div>
+        </form>
+        <div className="flex items-center space-x-2 py-4">
+          <input
+            type="checkbox"
+            id="hidePassword"
+            onChange={togglePasswordShown}
+            checked={passwordShown}
+            className="form-checkbox h-5 w-5 text-indigo-600"
+          />
+          <Label htmlFor="hidePassword">Show Password</Label>
         </div>
-      </div>
-    </section>
+      </CardContent>
+      <CardFooter className="flex flex-col items-center space-y-2">
+        {/* <Button variant="outline">Cancel</Button> */}
+        <Button onClick={handleLogin} className="w-full">
+          Login
+        </Button>
+        <Link href="/signup" className="text-xs hover:text-gray-300">
+          Create New Account
+        </Link>
+      </CardFooter>
+    </Card>
   );
-};
-
-export default LoginPage;
+}
