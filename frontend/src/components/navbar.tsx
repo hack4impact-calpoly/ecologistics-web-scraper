0a31709c9f0d80aa63aa78bccb3667d231254522
Hayley <haysho2260@gmail.com>
2024-02-06 20:32:02 -0800

    design: name bold, feat: logout, name

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,16 +1,41 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { signOut } from "next-auth/react";
+// import { signOut, useSession } from "next-auth/react";
+import { useState } from "react";
+import "@/styles/globals.css";
 
 export default function Navbar() {
+  // const { data: session } = useSession();
+  const [user, setUser] = useState("Karen");
+
+  const handleSignOut = async () => {
+    await signOut();
+  };
+
+  // const user = session?.user?.name || "Karen";
   return (
     <div className="bg-secondary p-4">
       <div className="container flex justify-between items-center">
-        <div className="font-semibold">
-          <Link href="/">Ecologistics Web Scraper</Link>
+        <div>
+          <Link href="/" className="flex items-left">
+            <div className="font-bold">ECOLO</div>
+            <div className="text-primary">GISTICS Web Scraper</div>
+          </Link>
+        </div>
+        <div className="text-center  text-primary">
+          <h3>Welcome {user}</h3>
         </div>
         <ul>
-          <li>
+          <li className="flex space-x-4">
             <Link href="/">Home</Link>
+            <button onClick={handleSignOut}>Logout</button>
+            {/* {session ? (
+              <button onClick={handleSignOut}>Logout</button>
+            ) : (
+              <Link href="/login">Login</Link>
+            )} */}
           </li>
         </ul>
       </div>
