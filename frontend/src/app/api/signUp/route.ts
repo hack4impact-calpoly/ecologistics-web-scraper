836d348dc1eb81cde118982a0d6af73d3bbd6af4
Sophia Peckner <sophiapeckner@gmail.com>
2024-02-08 17:19:28 -0800

    feat: add mongo db connection and create user

--- frontend/src/app/api/signUp/route.ts
+++ frontend/src/app/api/signUp/route.ts
@@ -1,10 +1,13 @@
+import connectDB from "@/database/db";
+import User from "@/database/userSchema";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const { email, password } = await req.json();
-    console.log("Email: ", email);
-    console.log("Password: ", password);
+    await connectDB();
+    await User.create({ email, password });
+
     return NextResponse.json({ message: "User registered." }, { status: 201 });
   } catch (error) {
     return NextResponse.json(
