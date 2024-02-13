26c7ca93509af1e17a37944393624c17273357e8
oli-lane <markus.oliver.lane@gmail.com>
2024-02-13 09:31:16 -0800

    chore: linting

--- backend/api/mongodb.py
+++ backend/api/mongodb.py
@@ -3,15 +3,16 @@ from dotenv import load_dotenv
 import os
 import certifi
 
+
 def get_mongo_client():
-    load_dotenv('../../.env.local')                         #allows access of env variables from .env
-    URI = os.getenv('MONGO_URI')
+    load_dotenv("../../.env.local")  # allows access of env variables from .env
+    URI = os.getenv("MONGO_URI")
 
     try:
         client = MongoClient(URI, tlsCAFile=certifi.where())
-        client.admin.command('ping')                        # confirm connection to db
+        client.admin.command("ping")  # confirm connection to db
     except Exception as e:
-            print(f"Error connecting to MongoDB: {e}")
-            client = None
-            
+        print(f"Error connecting to MongoDB: {e}")
+        client = None
+
     return client
