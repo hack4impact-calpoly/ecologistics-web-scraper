bd17b0c20bded2ecbd7ffab15f8b24c0053142c3
ryanchansf <ryanchansf@gmail.com>
2024-02-19 22:35:16 -0800

    Revert "add button to home page"
    This reverts commit 79b0af27e93c5e8070719b2d92475d3da6a649c1.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,7 @@
-"use client";
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  const handleScrape = () => {
-    // CHANGE THIS TO NAVIGATE TO NEW PAGE
-    console.log("change to scrape");
-  };
   return (
     <main>
       <h1>Home</h1>
-      <div>The button below will being scraping information from the site.</div>
-      <div className="flex justify-center h-screen">
-        <Button variant="outline" onClick={handleScrape}>
-          Begin Webscraping
-        </Button>
-      </div>
     </main>
   );
 }
