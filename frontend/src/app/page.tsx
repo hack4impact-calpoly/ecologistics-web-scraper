1a37589df8af3ed2d35e2f1d0f768a4e37ae5b39
Hayley <haysho2260@gmail.com>
2024-02-20 08:24:42 -0800

    add button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,20 @@
-import { ProposalTable } from "../components/proposalTable";
-import { useSession } from "next-auth/react";
+"use client";
+import { Button } from "@/components/ui/button";
 
-export default async function Home() {
+export default function Home() {
+  const handleScrape = () => {
+    // CHANGE THIS TO NAVIGATE TO NEW PAGE
+    console.log("change to scrape");
+  };
   return (
     <main>
       <h1>Home</h1>
-      <ProposalTable />
+      <div>The button below will being scraping information from the site.</div>
+      <div className="flex justify-center h-screen">
+        <Button variant="outline" onClick={handleScrape}>
+          Begin Webscraping
+        </Button>
+      </div>
     </main>
   );
 }
