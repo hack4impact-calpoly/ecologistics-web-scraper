6725f8ff319957329f9da03766c6b6b8412842d9
Isha_Varrier <ishavarrier@gmail.com>
2024-02-20 16:56:48 -0800

    fixing proposal table

--- frontend/src/components/proposalTable.tsx
+++ frontend/src/components/proposalTable.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
 
 import {
@@ -47,16 +47,6 @@ function DataTable<TData, TValue>({
     },
   });
 
-  const tableRef = useRef(null);
-  const [tableWidth, setTableWidth] = useState(null);
-
-  useEffect(() => {
-    if (tableRef.current) {
-      const width = tableRef.current.offsetWidth;
-      setTableWidth(width);
-    }
-  }, []);
-
   return (
     <div
       className="flex flex-col items-center justify-center text-center gap-4 mx-auto py-10"
@@ -71,7 +61,7 @@ function DataTable<TData, TValue>({
           Publication in Local/State Systems
         </p>
       </div>
-      <div className="rounded-md border" ref={tableRef}>
+      <div className="rounded-md border">
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
