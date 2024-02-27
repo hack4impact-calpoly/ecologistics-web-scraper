737d6adb945a5199e9d8fded0417949faeb93094
ryanchansf <ryanchansf@gmail.com>
2024-02-26 23:22:36 -0800

    fix: missing import

--- frontend/src/lib/tableColumns.tsx
+++ frontend/src/lib/tableColumns.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { IProposal } from "@/database/proposalSchema";
 
