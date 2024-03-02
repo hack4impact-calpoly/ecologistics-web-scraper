27586b8c74d9a0b8262e8f79994937b13e560d80
Ryan Chan <ryanchansf@gmail.com>
2024-03-02 15:56:59 -0800

    feat: init monterey county webscraping

--- backend/api/app.py
+++ backend/api/app.py
@@ -3,6 +3,7 @@ import mongodb
 from dotenv import load_dotenv
 from routes.slo_county import slo_county_blueprint
 from routes.sb_county import sb_county_blueprint
+from routes.monterey_county import monterey_county_blueprint
 from routes.example import example_blueprint
 
 
@@ -12,6 +13,7 @@ load_dotenv()
 app = Chalice(app_name="api")
 app.register_blueprint(slo_county_blueprint)
 app.register_blueprint(sb_county_blueprint)
+app.register_blueprint(monterey_county_blueprint)
 app.register_blueprint(example_blueprint)
 
 client = mongodb.get_mongo_client()
