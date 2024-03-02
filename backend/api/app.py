f4cf19e17ada7c1710825ef2ee17865db1db24a7
oli-lane <markus.oliver.lane@gmail.com>
2024-03-02 15:55:10 -0800

    feat: sb county be setup

--- backend/api/app.py
+++ backend/api/app.py
@@ -2,6 +2,7 @@ from chalice import Chalice
 import mongodb
 from dotenv import load_dotenv
 from routes.slo_county import slo_county_blueprint
+from routes.sb_county import sb_county_blueprint
 from routes.example import example_blueprint
 
 
@@ -10,6 +11,7 @@ load_dotenv()
 
 app = Chalice(app_name="api")
 app.register_blueprint(slo_county_blueprint)
+app.register_blueprint(sb_county_blueprint)
 app.register_blueprint(example_blueprint)
 
 client = mongodb.get_mongo_client()
