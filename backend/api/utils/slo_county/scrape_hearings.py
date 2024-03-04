d7a7623bac197e13988acb42e5d49f8edeca9e3c
Wesley Tam <wesleytam88@gmail.com>
2024-03-03 17:54:59 -0800

    feat: generate unique id for meetings using date

--- backend/api/utils/slo_county/scrape_hearings.py
+++ backend/api/utils/slo_county/scrape_hearings.py
@@ -1,5 +1,7 @@
 import requests
+import time
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 
 def scrape_hearings():
@@ -7,29 +9,51 @@ def scrape_hearings():
     response = requests.get(url)
 
     soup = BeautifulSoup(response.content, "html.parser")
-    table = soup.find("table", class_="listingTable")
+    upcoming_events_table = soup.find("table", class_="listingTable")
     
     detail_links = []
+    detail_ids = []
 
     # Check if the table is found
-    if table:
-        # Find all <a> tags within <td> tags with class "listItem" and headers "ItemDocumentsUpcoming"
-        item_detail_links = table.find_all(
-            'td', class_='listItem', headers='ItemDocumentsUpcoming')
+    if upcoming_events_table:
+        # Find all <tr> tags within the upcoming events table
+        events = upcoming_events_table.find_all('tr', class_='listingRow')
 
-        # Extract the href attribute from each <a> tag
-        for link in item_detail_links:
-            a_tag = link.find('a')
-            if a_tag:
-                detail_links.append(a_tag['href'])
+        for event in events:
+            link = event.find(
+                'td', class_='listItem', headers='ItemDocumentsUpcoming')
+
+            if link:
+                # Extract the href attribute from each <a> tag
+                a_tag = link.find('a')
+                if a_tag:
+                    detail_links.append(a_tag['href'])
+
+                    # Generate unique ID based on time of event
+                    date = event.find('td', class_='listItem', headers='Date')
+                    date_string = date.get_text(strip=True)
+                    id = date_to_unix(date_string)
+                    detail_ids.append(id)
 
         # Print the extracted links
-        for link in detail_links:
+        for i, link in enumerate(detail_links):
             print(link)
+            print(detail_ids[i])
+
     else:
         print("Table not found or empty.")
 
     return detail_links
+
+
+def date_to_unix(date: str) -> int:
+    '''
+    Given a date string of format "January 1, 2024 - 9:00 AM",
+    return its unix timestamp
+    '''
+    date_object = datetime.strptime(date, "%B %d, %Y - %I:%M %p")
+    unix_time = time.mktime(date_object.timetuple())
+    return int(unix_time)
 
 
 scrape_hearings()
