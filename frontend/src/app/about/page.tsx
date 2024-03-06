eca2a94ad0cbfe6e69878bab5aef51202fc0e100
oli-lane <markus.oliver.lane@gmail.com>
2024-03-06 15:09:53 -0800

    cleanup navbar, footer, about

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,26 +1,21 @@
 export default function About() {
   return (
-    <main>
-      <div className="w-full">
-        <div className="flex flex-col items-center justify-center py-3 py-3">
-          <div
-            className="bg-gray-200 mb-3"
-            style={{ fontSize: "250%", fontWeight: "bold" }}
-          >
-            <h1>About Ecologistics</h1>
-          </div>
-
-          <div className="mb-3">
-            <h2>Data Source #1</h2>
-            <h2>Data Source #2</h2>
-            <h2>Data Source #3</h2>
-          </div>
-
-          <div className="mb -3">
-            <p>information on how to use each source</p>
-          </div>
-        </div>
+    <div className="flex flex-col w-full items-center justify-center py-3 py-3 gap-3">
+      <div className="bg-gray-200 w-full p-3 text-center rounded-lg">
+        <h1 className="text-5xl font-bold">
+          About the Ecologistics Web Scraper
+        </h1>
       </div>
-    </main>
+
+      <div className="mb-3">
+        <h2>Data Source #1</h2>
+        <h2>Data Source #2</h2>
+        <h2>Data Source #3</h2>
+      </div>
+
+      <div className="mb -3">
+        <p>information on how to use each source</p>
+      </div>
+    </div>
   );
 }
