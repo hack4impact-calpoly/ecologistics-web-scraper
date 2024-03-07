d3ef6c99161efb0b2eede134ed29443a9deffa42
oli-lane <markus.oliver.lane@gmail.com>
2024-03-06 16:13:01 -0800

    feat: table description

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,11 +5,14 @@ import { useSession } from "next-auth/react";
 export default function Home() {
   return (
     <div className="flex flex-col w-ful gap-5">
-      <div className="flex flex-col items-center justify-center bg-primary-foreground py-3 px-3 rounded-lg">
+      <div className="flex flex-col items-center justify-center bg-primary-foreground py-3 px-40 rounded-lg gap-3">
         <h1 className="text-3xl font-bold">Public Hearings Table</h1>
-        <p>
-          Development Project Approval Milestones that Trigger Document
-          Publication in Local/State Systems
+        <p className="text-center">
+          This table compiles projects extracted via web scraping from a
+          county-wide scheduling website, specifically tracking the date a given
+          project will be discussed in an upcoming public hearing. It includes
+          fields for the project name, county file number, location, hearing
+          date, and State Clearing House Number (if applicable).
         </p>
       </div>
       <div>
