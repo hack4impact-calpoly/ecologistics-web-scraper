38faaca741e743628d56607b356ae7f256c58415
oli-lane <markus.oliver.lane@gmail.com>
2024-03-06 17:06:46 -0800

    feat: populate about page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -20,7 +20,7 @@ export default function About() {
 
   return (
     <div className="flex flex-col w-full gap-10">
-      <div className="flex flex-col bg-primary-foreground rounded-lg w-full items-center justify-center p-5 gap-5">
+      <div className="flex flex-col bg-primary-foreground rounded-lg w-full items-center justify-center py-5 px-20 gap-5">
         <h1 className="text-3xl font-bold">Data Sources</h1>
 
         <div className="grid grid-cols-2 gap-10">
@@ -68,11 +68,60 @@ export default function About() {
           </Card>
         </div>
       </div>
-      <div className="flex flex-col bg-primary-foreground rounded-lg w-full items-center justify-center p-5">
+      <div className="flex flex-col bg-primary-foreground rounded-lg w-full items-center justify-center py-5 px-20 gap-5">
         <h1 className="text-3xl font-bold">Web Scraping Approach</h1>
+        <Card className="flex flex-col w-full h-[200px]">
+          <CardHeader>
+            <CardTitle className="text-center">
+              San Luis Obispo County
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>
+              <span className="font-bold">Abstract: </span>Get projects from SLO
+              County Meetings Calendar, cross-reference in State Clearing House,
+              display in table.
+            </p>
+            <p>
+              <span className="font-bold">Details:</span> Scrape SLO County
+              meetings calender once a week. For each meeting, scrape the agenda
+              and identify agenda items resembling a project hearing. For each
+              project, identify the County File Number and cross-reference it in
+              the State Clearing House. If a match is found, identify the
+              project&apos;s unique SCH# and scrape additional information from
+              the projects page.
+            </p>
+          </CardContent>
+        </Card>
       </div>
-      <div className="flex flex-col bg-primary-foreground rounded-lg w-full items-center justify-center p-5">
+      <div className="flex flex-col bg-primary-foreground rounded-lg w-full items-center justify-center py-5 px-20 gap-5">
         <h1 className="text-3xl font-bold">Statistics</h1>
+        <div className="grid grid-cols-3 gap-10">
+          <Card className="flex flex-col justify-between w-[350px] h-[300px]">
+            <CardHeader>
+              <CardTitle className="text-center">Total Projects</CardTitle>
+            </CardHeader>
+            <CardContent className="flex h-full justify-center items-center">
+              <h1 className="text-7xl">--</h1>
+            </CardContent>
+          </Card>
+          <Card className="flex flex-col justify-between w-[350px] h-[300px]">
+            <CardHeader>
+              <CardTitle className="text-center">% Connected to SCH</CardTitle>
+            </CardHeader>
+            <CardContent className="flex h-full justify-center items-center">
+              <h1 className="text-7xl">--</h1>
+            </CardContent>
+          </Card>
+          <Card className="flex flex-col w-[350px] h-[300px]">
+            <CardHeader>
+              <CardTitle className="text-center">Total Hearings</CardTitle>
+            </CardHeader>
+            <CardContent className="flex h-full justify-center items-center">
+              <h1 className="text-7xl">--</h1>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
