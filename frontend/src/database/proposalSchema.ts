62257c069ce0c2ac0d642d08b6bbed003b75acde
Wesley Tam <wesleytam88@gmail.com>
2024-04-15 23:05:18 -0700

    feat: change to new column names

--- frontend/src/database/proposalSchema.ts
+++ frontend/src/database/proposalSchema.ts
@@ -2,28 +2,34 @@ import mongoose, { Schema } from "mongoose";
 
 //typescript Interface
 export type IProposal = {
-  name: string;
-  link: string;
-  date_approved: Date;
-  date_closed: Date;
+  county_file_number: string;
+  hearing_date: Date;
   review_status: string;
-  flag_status: string;
   location: string;
-  meeting_date: Date;
-  public_comment_end_date: Date;
+  apn: string;
+  date_accepted: Date;
+  requesting_party: string;
+  sch_number: string;
+  title: string;
+  public_hearing_agenda_link: string;
+  sch_page_link: string;
+  additional_notes: string;
 };
 
 //mongoose schema
 export const proposalSchema = new Schema<IProposal>({
-  name: { type: String, required: true },
-  link: { type: String, required: true },
-  date_approved: { type: Date, required: false, default: new Date() },
-  date_closed: { type: Date, required: false, default: new Date() },
+  county_file_number: { type: String, required: true },
+  hearing_date: { type: Date, required: true },
   review_status: { type: String, required: true },
-  flag_status: { type: String, required: true },
   location: { type: String, required: true },
-  meeting_date: { type: Date, required: false, default: new Date() },
-  public_comment_end_date: { type: Date, required: false, default: new Date() },
+  apn: { type: String, required: true },
+  date_accepted: { type: Date, required: true },
+  requesting_party: { type: String, required: false },
+  sch_number: { type: String, required: true },
+  title: { type: String, required: true },
+  public_hearing_agenda_link: { type: String, required: true },
+  sch_page_link: { type: String, required: false },
+  additional_notes: { type: String, required: true },
 });
 
 // defining the collection and model and creating one if doesn't exist
