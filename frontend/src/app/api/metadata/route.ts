202a51a7020668eac64e046570c1972c6950298a
Ryan Chan <ryanchansf@gmail.com>
2024-05-07 09:52:02 -0700

    fix: dynamic rendering of metadata route

--- frontend/src/app/api/metadata/route.ts
+++ frontend/src/app/api/metadata/route.ts
@@ -2,9 +2,18 @@ import connectDB from "@/database/db";
 import ScraperMetadata from "@/database/metadataSchema";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  await connectDB();
+export async function GET(req: Request) {
+  // DUMMY CODE — meant to handle dynamic rendering
+  const url = new URL(req.url);
+  const dummy = url.searchParams.get("dummy");
+
   try {
+    await connectDB();
+
+    if (dummy) {
+      console.log(dummy);
+    }
+
     const metadata = await ScraperMetadata.findOne().orFail();
     return NextResponse.json(metadata);
   } catch (error) {
