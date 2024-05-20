84846ca1ab578ddaaf311fbc571545558bed7a72
Isha_Varrier <ishavarrier@gmail.com>
2024-05-20 13:17:19 -0700

    commiting toast

--- frontend/src/components/ui/toast.tsx
+++ frontend/src/components/ui/toast.tsx
@@ -32,6 +32,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
+        green: "border-green-600 bg-green-600 text-green-100",
       },
     },
     defaultVariants: {
