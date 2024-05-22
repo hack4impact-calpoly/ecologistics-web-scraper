888db099564978ffece34b0e068d555ed096a509
Ryan Chan <ryanchansf@gmail.com>
2024-05-21 17:43:13 -0700

    chore: comment for future development

--- frontend/src/components/scrapeButton.tsx
+++ frontend/src/components/scrapeButton.tsx
@@ -40,7 +40,7 @@ export default function ScrapeButton() {
         duration: 3000,
       });
       // refresh on success
-      // window.location.reload();
+      // window.location.reload(); TODO: uncomment this line to update table
     } catch (error) {
       const county = "San Luis Obispo";
       toast({
