6b7067bbb5a2966c0d3346516dd3f339ccfd75ba
Sophia Peckner <sophiapeckner@gmail.com>
2024-05-21 20:00:03 -0700

    currently PUT request returns success but doesn't actually update the db

--- frontend/src/app/api/projects/route.ts
+++ frontend/src/app/api/projects/route.ts
@@ -21,3 +21,44 @@ export async function GET(req: Request) {
     return NextResponse.json("Error retrieving projects.", { status: 404 });
   }
 }
+
+export async function PUT(req: Request) {
+  const { countyFileNumber, reviewStatus, additionalNotes } = await req.json();
+
+  try {
+    await connectDB();
+
+    const possibleStatus = ["Reviewed", "In Review", "Unreviewed"];
+    const fieldMising = !countyFileNumber || !reviewStatus;
+    const statusPossible = possibleStatus.includes(reviewStatus);
+
+    if ((fieldMising || !statusPossible) && !additionalNotes) {
+      return NextResponse.json("Missing or bad field in request");
+    }
+
+    if (reviewStatus) {
+      await Project.findOneAndUpdate(
+        { county_file_number: countyFileNumber },
+        { review_status: reviewStatus },
+      );
+      return NextResponse.json("Update review status success", { status: 200 });
+    } else if (additionalNotes) {
+      await Project.findOneAndUpdate(
+        { county_file_number: countyFileNumber },
+        { $set: { additional_notes: additionalNotes } },
+        (err: any) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("success");
+          }
+        },
+      );
+      return NextResponse.json("Update additional notes success", {
+        status: 200,
+      });
+    }
+  } catch (error) {
+    return NextResponse.json("Error updating review status", { status: 500 });
+  }
+}
