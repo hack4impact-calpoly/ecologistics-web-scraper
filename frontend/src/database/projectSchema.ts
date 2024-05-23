0aa4d1f047641aad429d3c1803d13138b8c3f670
James <jamesktorres2022@gmail.com>
2024-05-22 21:51:29 -0700

    fix: updated project schema to match database. fix routes

--- frontend/src/database/projectSchema.ts
+++ frontend/src/database/projectSchema.ts
@@ -2,34 +2,34 @@ import mongoose, { Schema } from "mongoose";
 
 //typescript Interface
 export type IProject = {
-  countyFileNumber: string;
-  hearingDate: Date;
-  reviewStatus: string;
+  county_file_number: string;
+  hearing_date: string;
+  review_status: string;
   location: string;
   apn: string;
-  dateAccepted: Date;
-  requestingParty?: string;
-  schNumber?: number;
+  date_accepted: string;
+  requesting_party?: string;
+  sch_number?: number;
   title?: string;
-  publicHearingAgenda: string;
-  schLink?: string;
-  additionalNotes?: string;
+  public_hearing_agenda_link: string;
+  sch_page_link?: string;
+  additional_notes?: string;
 };
 
 //mongoose schema
 export const projectSchema = new Schema<IProject>({
-  countyFileNumber: { type: String, required: true },
-  hearingDate: { type: Date, required: true },
-  reviewStatus: { type: String, required: true },
+  county_file_number: { type: String, required: true },
+  hearing_date: { type: String, required: true },
+  review_status: { type: String, required: true },
   location: { type: String, required: true },
   apn: { type: String, required: true },
-  dateAccepted: { type: Date, required: true },
-  requestingParty: { type: String, required: false, default: "N/A" },
-  schNumber: { type: Number, required: false, default: -1 },
+  date_accepted: { type: String, required: true },
+  requesting_party: { type: String, required: false, default: "N/A" },
+  sch_number: { type: Number, required: false, default: -1 },
   title: { type: String, required: false, default: "N/A" },
-  publicHearingAgenda: { type: String, required: true },
-  schLink: { type: String, required: false, default: "N/A" },
-  additionalNotes: { type: String, required: false, default: "N/A" },
+  public_hearing_agenda_link: { type: String, required: true },
+  sch_page_link: { type: String, required: false, default: "N/A" },
+  additional_notes: { type: String, required: false, default: "N/A" },
 });
 
 // defining the collection and model and creating one if doesn't exist
