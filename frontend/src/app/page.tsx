761e372c1e556a49fe152427881e1916785796f5
oli-lane <markus.oliver.lane@gmail.com>
2024-05-27 14:32:49 -0700

    feat: table info dialog

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,6 +16,16 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const [projectData, setProjectData] = useState<ReformattedProject[]>([]);
@@ -58,50 +68,50 @@ export default function Home() {
 
   return (
     <div className="flex flex-col w-ful gap-5">
-      <div className="flex flex-col items-center justify-center bg-primary-foreground py-3 px-40 rounded-lg">
+      <div className="flex flex-col items-center justify-center bg-primary-foreground py-3 px-40 rounded-lg gap-3">
         <h1 className="text-3xl font-bold">Public Hearings Table</h1>
-
-        <Collapsible>
-          <div className="w-full flex justify-center mt-5">
-            <CollapsibleTrigger>
-              <h1 style={{ textDecoration: "underline", color: "grey" }}>
-                Click here for more information on how to use this table
-              </h1>
-            </CollapsibleTrigger>
-          </div>
-          <CollapsibleContent>
-            <p className="text-center">
-              This table compiles projects extracted via web scraping from a
-              county-wide scheduling website, specifically tracking the date a
-              given project will be discussed in an upcoming public hearing. It
-              includes fields for the project name, county file number,
-              location, hearing date, and State Clearing House Number (if
-              exists).
-            </p>
-            <Accordion type="single" collapsible className="w-full">
-              <AccordionItem value="item-1">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-2">
-                <AccordionTrigger>Is it styled?</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It comes with default styles that matches the other
-                  components&apos; aesthetic.
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-3">
-                <AccordionTrigger>Is it animated?</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It&apos;s animated by default, but you can disable it if
-                  you prefer.
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </CollapsibleContent>
-        </Collapsible>
+        <Dialog>
+          <DialogTrigger asChild>
+            <a
+              style={{
+                color: "grey",
+                cursor: "pointer",
+                textDecoration: "underline",
+              }}
+            >
+              <p>Click here to learn more about how to use this table</p>
+            </a>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <Accordion type="single" collapsible className="w-full">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                  <AccordionTrigger>Is it styled?</AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It comes with default styles that matches the other
+                    components&apos; aesthetic.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                  <AccordionTrigger>Is it animated?</AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It&apos;s animated by default, but you can disable it
+                    if you prefer.
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
       <div>
         <div className="flex justify-center">
@@ -109,6 +119,13 @@ export default function Home() {
         </div>
         <ProjectTable projectData={projectData} numProjects={numProjects} />
       </div>
+      <p className="text-center">
+        This table compiles projects extracted via web scraping from a
+        county-wide scheduling website, specifically tracking the date a given
+        project will be discussed in an upcoming public hearing. It includes
+        fields for the project name, county file number, location, hearing date,
+        and State Clearing House Number (if exists).
+      </p>
     </div>
   );
 }
