ae6d756582f041d2415fb8345b5a0e7f12a27a04
Ryan Chan <ryanchansf@gmail.com>
2024-05-30 16:44:07 -0700

    style: signup card positioning

--- frontend/src/app/(auth)/signup/page.tsx
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -113,85 +113,91 @@ export default function SignUpPage() {
   };
 
   return (
-    <Card className="w-[370px] p-6 mx-auto">
-      <CardHeader>
-        <CardTitle className="text-2xl text-gray-700 pb-1 text-center">
-          Sign Up
-        </CardTitle>
-        {/* <CardDescription>Deploy your new project in one-click.</CardDescription> */}
-      </CardHeader>
-      <CardContent className="pb-4">
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Email</Label>
-              <Input
-                id="name"
-                placeholder="Email"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                  handleInputChange();
-                }}
-              />
+    <div className="flex items-center justify-center min-h-screen mt-[-100px] mb-[-100px]">
+      <Card className="w-[370px] p-6 mx-auto">
+        <CardHeader>
+          <CardTitle className="text-2xl text-gray-700 pb-1 text-center">
+            Sign Up
+          </CardTitle>
+          {/* <CardDescription>Deploy your new project in one-click.</CardDescription> */}
+        </CardHeader>
+        <CardContent className="pb-4">
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Email</Label>
+                <Input
+                  id="name"
+                  placeholder="Email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    handleInputChange();
+                  }}
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  // text = show password, password = no show password
+                  type={passwordShown ? "text" : "password"}
+                  placeholder="Password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    handleInputChange();
+                  }}
+                />
+                <span className="text-xs text-gray-500">
+                  Password must be at least 8 characters long and contain a
+                  number.
+                </span>
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="confirm-password">Confirm Password</Label>
+                <Input
+                  id="confirm-password"
+                  type={confirmpasswordShown ? "text" : "password"}
+                  placeholder="Confirm Password"
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                    handleInputChange();
+                  }}
+                />
+              </div>
             </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                // text = show password, password = no show password
-                type={passwordShown ? "text" : "password"}
-                placeholder="Password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                  handleInputChange();
-                }}
-              />
-              <span className="text-xs text-gray-500">
-                Password must be at least 8 characters long and contain a
-                number.
-              </span>
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="confirm-password">Confirm Password</Label>
-              <Input
-                id="confirm-password"
-                type={confirmpasswordShown ? "text" : "password"}
-                placeholder="Confirm Password"
-                onChange={(e) => {
-                  setConfirmPassword(e.target.value);
-                  handleInputChange();
-                }}
-              />
-            </div>
+          </form>
+          <div className="flex items-center space-x-2 py-4">
+            <input
+              type="checkbox"
+              id="hidePassword"
+              onChange={togglePasswordShown}
+              checked={passwordShown}
+              className="form-checkbox h-5 w-5 text-indigo-600"
+            />
+            <Label htmlFor="hidePassword">Show Password</Label>
           </div>
-        </form>
-        <div className="flex items-center space-x-2 py-4">
-          <input
-            type="checkbox"
-            id="hidePassword"
-            onChange={togglePasswordShown}
-            checked={passwordShown}
-            className="form-checkbox h-5 w-5 text-indigo-600"
-          />
-          <Label htmlFor="hidePassword">Show Password</Label>
-        </div>
-      </CardContent>
-      {errorMessage && (
-        <Alert variant="destructive">
-          <ExclamationTriangleIcon className="h-4 w-4" />
-          <AlertTitle>Error</AlertTitle>
-          <AlertDescription>{errorMessage}</AlertDescription>
-        </Alert>
-      )}
-      <CardFooter className="flex flex-col items-center space-y-2 mt-4">
-        {/* <Button variant="outline">Cancel</Button> */}
-        <Button onClick={handleSignUp} className="w-full" variant={"secondary"}>
-          Sign Up
-        </Button>
-        <Link href="/login" className="text-xs hover:text-gray-300">
-          Already have an account? Log In
-        </Link>
-      </CardFooter>
-    </Card>
+        </CardContent>
+        {errorMessage && (
+          <Alert variant="destructive">
+            <ExclamationTriangleIcon className="h-4 w-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>{errorMessage}</AlertDescription>
+          </Alert>
+        )}
+        <CardFooter className="flex flex-col items-center space-y-2 mt-4">
+          {/* <Button variant="outline">Cancel</Button> */}
+          <Button
+            onClick={handleSignUp}
+            className="w-full"
+            variant={"secondary"}
+          >
+            Sign Up
+          </Button>
+          <Link href="/login" className="text-xs hover:text-gray-300">
+            Already have an account? Log In
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
